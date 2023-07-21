8c2a50eacc40b59f3b76f6a67dc71fc75dca83f3
Nora Söderlund <norasoderlund@icloud.com>
2023-07-22 00:14:05 +0200

    Added speed endpoint

--- src/domains/router.ts
+++ src/domains/router.ts
@@ -3,6 +3,7 @@ import { handleActivityRouteRequest } from "../routes/activities/[activityId]/ro
 import { handleActivitySessionsRequest } from "../routes/activities/[activityId]/sessions";
 import { withAuth } from "@ridetracker/authservice";
 import { handleActivitySessionsAltitudeRequest } from "../routes/activities/[activityId]/sessions/altitude";
+import { handleActivitySessionsSpeedRequest } from "../routes/activities/[activityId]/sessions/speed";
 
 export default function createRouter() {
     const router = ThrowableRouter();
@@ -10,6 +11,7 @@ export default function createRouter() {
     router.get("/activities/:activityId/route", withAuth("user", "DATABASE"), withParams, handleActivityRouteRequest);
     router.get("/api/activities/:activityId/sessions", withAuth("user", "DATABASE"), withParams, handleActivitySessionsRequest);
     router.get("/api/activities/:activityId/sessions/altitude", withAuth("user", "DATABASE"), withParams, handleActivitySessionsAltitudeRequest);
+    router.get("/api/activities/:activityId/sessions/speed", withAuth("user", "DATABASE"), withParams, handleActivitySessionsSpeedRequest);
 
     return router;
 };
---
+++ src/routes/activities/[activityId]/sessions/speed.ts
@@ -0,0 +1,95 @@
+import getActivitySessions from "../../../../controllers/activities/getActivitySessions";
+import { Coordinate } from "../../../../models/Coordinate";
+
+export async function handleActivitySessionsSpeedRequest(request: CfRequest, env: Env) {
+    const { activityId } = request.params;
+
+    const sessions = await getActivitySessions(env.BUCKET, activityId);
+
+    if(!sessions)
+        return Response.json({ success: false });
+
+    const significantChange = 4;
+
+    const polylines = sessions.map((session) => {
+        const points: {
+            coordinate: Coordinate;
+            speed: number;
+        }[] = [];
+
+        const start = session.locations[0];
+        const end = session.locations[1];
+
+        points.push({
+            coordinate: {
+                latitude: start.coords.latitude,
+                longitude: start.coords.longitude
+            },
+
+            speed: start.coords.speed
+        });
+
+        for(let index = 0; index < session.locations.length - 1; index++) {
+            const location = session.locations[index];
+            const previous = points[points.length - 1];
+
+            const difference = Math.abs(location.coords.speed - previous.speed);
+
+            if(difference < significantChange) {
+                console.log(`Skipping speed change because ${difference} is less than significant change of ${significantChange}`);
+
+                continue;
+            }
+            
+            points.push({
+                coordinate: {
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude
+                },
+
+                speed: location.coords.speed
+            });
+        }
+            
+        points.push({
+            coordinate: {
+                latitude: end.coords.latitude,
+                longitude: end.coords.longitude
+            },
+
+            speed: end.coords.speed
+        });
+
+        return points;
+    });
+
+    const speeds = polylines.flatMap((points) => points.map((point) => point.speed));
+
+    const sum = speeds.reduce((accumulated, speed) => accumulated + speed, 0);
+    const average = sum / speeds.length;
+
+    return Response.json({
+        success: true,
+
+        speeds: {
+            minimum: Math.min(...speeds),
+            maximum: Math.max(...speeds),
+            average
+        },
+
+        polylines: polylines.map((points) => {
+            return {
+                points: points.map((point) => {
+                    return {
+                        coordinate: {
+                            latitude: point.coordinate.latitude,
+                            longitude: point.coordinate.longitude
+                        },
+
+                        speed: point.speed
+                    };
+                })
+            };
+        })
+    });
+};
